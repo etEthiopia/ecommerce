b186ddab46a554b264f457fa8a2b612596c33364
etEthiopia <daginegussu@gmail.com>
2020-04-15 14:56:46 +0300

    so so hard, what a relief

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import 'package:ecommerceapp/pages/cart.dart';
 import 'package:ecommerceapp/pages/home.dart';
 import 'package:ecommerceapp/pages/product_details.dart';
@@ -13,11 +12,4 @@ void main() {
       "/mycart": (context) => CartPage()
     },
   ));
-=======
-import 'package:ecommerceapp/pages/home.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomePage()));
->>>>>>> aa5ec2d01343c02276ff5a709ec99da40f06639e
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
 import 'package:ecommerceapp/components/categories_hlistview.dart';
 import 'package:ecommerceapp/components/image_carousel.dart';
 import 'package:ecommerceapp/components/menu_drawer.dart';
 import 'package:ecommerceapp/components/products_grid.dart';
-=======
->>>>>>> aa5ec2d01343c02276ff5a709ec99da40f06639e
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -17,10 +14,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-<<<<<<< HEAD
         elevation: 0.5,
-=======
->>>>>>> aa5ec2d01343c02276ff5a709ec99da40f06639e
         title: Text('Fash App'),
         backgroundColor: Colors.red,
         actions: <Widget>[
@@ -29,7 +23,6 @@ class _HomePageState extends State<HomePage> {
           IconButton(
               icon: Icon(Icons.shopping_cart),
               color: Colors.white,
-<<<<<<< HEAD
               onPressed: () {
                 Navigator.of(context).pushNamed("/mycart");
               })
@@ -63,80 +56,6 @@ class _HomePageState extends State<HomePage> {
           // The ProductsGrid in Cards
           Container(height: 200, child: ProductsGrid())
         ],
-=======
-              onPressed: () {})
-        ],
-      ),
-      drawer: Drawer(
-        child: ListView(children: <Widget>[
-          new UserAccountsDrawerHeader(
-            accountName: Text("Dagmawi Negussu"),
-            accountEmail: Text("daginegussu@gmail.com"),
-            currentAccountPicture: GestureDetector(
-              child: CircleAvatar(
-                  backgroundImage: AssetImage('assets/dagidark.JPG')),
-            ),
-            decoration: BoxDecoration(color: Colors.red),
-          ),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text("Home Page"),
-                  leading: Icon(
-                    Icons.home,
-                    color: Colors.red,
-                  ))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text("My Account"),
-                  leading: Icon(
-                    Icons.person,
-                    color: Colors.red,
-                  ))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text("My Orders"),
-                  leading: Icon(
-                    Icons.shopping_basket,
-                    color: Colors.red,
-                  ))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text("Categories"),
-                  leading: Icon(
-                    Icons.dashboard,
-                    color: Colors.red,
-                  ))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text("Favorites"),
-                  leading: Icon(
-                    Icons.favorite,
-                    color: Colors.red,
-                  ))),
-          Divider(),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text("Settings"),
-                  leading: Icon(
-                    Icons.settings,
-                    color: Colors.red,
-                  ))),
-          InkWell(
-              onTap: () {},
-              child: ListTile(
-                  title: Text("About"),
-                  leading: Icon(
-                    Icons.help,
-                    color: Colors.red,
-                  )))
-        ]),
->>>>>>> aa5ec2d01343c02276ff5a709ec99da40f06639e
       ),
     );
   }
